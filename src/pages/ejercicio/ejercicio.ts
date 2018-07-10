e67b7f3ff322f10eb667a22152d4293c17ca0ed4
heiner <heiner96@gmail.com>
2018-07-10 14:34:23 -0600

    antes de agregar el plugin de health, todo funca

--- src/pages/ejercicio/ejercicio.ts
+++ src/pages/ejercicio/ejercicio.ts
@@ -18,12 +18,25 @@ export class EjercicioPage {
 	ejercicio : Ejercicio;
   constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController) {  	
   	this.ejercicio=this.navParams.get('ejercicio');
-  	console.log(this.ejercicio); 
+  	this.revisarTiempo(this.ejercicio); 
   }
 
   ionViewWillLoad() {  
   }
   hide(){
   	this.view.dismiss();
+  }
+  revisarTiempo(ejercicio : Ejercicio){
+	if (ejercicio.hasOwnProperty('tiempo')) { 
+		if(ejercicio.tiempo!=0){
+			  		//es ejercicio por tiempo
+		}
+		else{
+			  		//no es un ejercicio por tiempo
+		}
+	}  	
+	else{
+		//no tiene tiempo
+	}  	
   }
 }
